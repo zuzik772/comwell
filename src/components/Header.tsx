6ffc7e0f441c6db8e94574c772958f13c26ca000
Tsukani <34612305+Tsukani@users.noreply.github.com>
2023-11-06 15:02:19 +0100

    Header essentials

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../img/Logo.svg";
 import Image from "next/image";
+import { BiChevronDown } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import { HiMenu } from "react-icons/hi";
 
 export const Header = () => {
+  const [navbarTransparent, setNavbarTransparent] = useState(true);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setNavbarTransparent(window.scrollY < 100);
+    };
+  }, []);
+
   return (
-    <header>
-      <nav>
+    <header
+      className={`z-50 fixed w-full duration-200 ${
+        navbarTransparent ? "bg-transparent" : "bg-secondary"
+      }`}
+    >
+      <nav
+        className={`${
+          navbarTransparent ? "invert" : "invert-0"
+        } duration-200 w-full p-6 flex justify-between`}
+        style={{
+          fontFamily: "Fellix-SemiBold",
+        }}
+      >
         <a>
           <Image src={logo} alt={""} />
         </a>
-        <ul>
-          <li>Locations</li>
-          <li>Profile</li>
-          <li>Menu</li>
+        <ul className="flex gap-8">
+          <li className="flex items-center gap-2">
+            Locations <BiChevronDown className="text-xl" />
+          </li>
+          <li className="flex items-center gap-2">
+            Profile <FaRegUser className="text-xl" />
+          </li>
+          <li className="flex items-center gap-2">
+            Menu <HiMenu className="text-xl" />
+          </li>
         </ul>
       </nav>
     </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
   );
 };
