d7e93a1ef7a58f528d10eeabbb9c1ea52a9077d8
Tsukani <34612305+Tsukani@users.noreply.github.com>
2023-11-06 13:09:29 +0100

    Last additions before branching out

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Header } from "../components/Header";
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Header />
         <Main />
         <NextScript />
       </body>
