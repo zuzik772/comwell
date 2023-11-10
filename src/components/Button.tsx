d9fd6e1161b2ac187dc646b544d0a154b612e37a
Tsukani <34612305+Tsukani@users.noreply.github.com>
2023-11-10 18:40:04 +0100

    Made children an optional prop on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 type ButtonProps = {
   onClick?: () => void;
-  children: ReactNode;
+  children?: ReactNode;
 };
 
 export const Button: FC<ButtonProps> = ({ onClick, children }) => {
