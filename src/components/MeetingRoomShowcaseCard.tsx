9588af15a200e8c57e68dc08833e46b18edbdfb9
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2023-12-19 17:22:32 +0100

    meeting rooms showcase cards

--- src/components/MeetingRoomShowcaseCard.tsx
+++ src/components/MeetingRoomShowcaseCard.tsx
@@ -15,18 +15,25 @@ export const MeetingRoomsShowcaseCard: FC<MeetingRoomsShowcaseCardProps> = ({
       className="flex h-56 rounded-lg border border-gray-300 cursor-pointer"
       onClick={onClick}
     >
-      <section className="w-1/3 h-full flex flex-col p-4 relative gap-2">
-        {/* <p className="text-2xl font-semibold">{meetingRoom.name}</p> */}
-        <p className="text-sm overflow-hidden line-clamp-2 ">
-          {meetingRoom.maxCapacity}
-        </p>
-        <p className="text-sm overflow-hidden line-clamp-2 ">
-          {meetingRoom.description}
-        </p>
-        <p className="text-sm overflow-hidden line-clamp-2 ">
-          {meetingRoom.bulletPoints}
-        </p>
-      </section>
+      <div className="flex gap-4 px-12 py-2 ">
+        <section className="w-1/2">
+          <img
+            src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/m%C3%B8der_konferencer/comwell_moede-og-konference41.jpg/636f34e0fcca9da5c8a8dde859a5bf2f.webp"
+            alt="meeting room"
+          />
+        </section>
+        <section className="w-1/2 flex flex-col gap-2">
+          <p>{meetingRoom?.description}</p>
+          <p>
+            Meeting room capacity: <i>{meetingRoom?.maxCapacity} people</i>
+          </p>
+          <ul className="flex flex-col pl-8">
+            {meetingRoom?.bulletPoints.map((bulletPoint) => (
+              <li className="list-disc">{bulletPoint}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 };
--- src/components/Menus/RequestMenu.tsx
+++ src/components/Menus/RequestMenu.tsx
@@ -1,18 +1,17 @@
 import React, { FC, useEffect, useState } from "react";
 import { Menu } from "./Menu";
-import { useHotelSearchStore } from "@/stores/hotelSearchStore";
 import { MeetingRoom } from "@/types/MeetingRoom";
 import { useMenuControllerStore } from "@/stores/menuControllerStore";
 import { MeetingRoomsShowcaseCard } from "../MeetingRoomShowcaseCard";
 import { LoadingSpinner } from "../LoadingSpinner";
 import { useSearchMenuControllerStore } from "@/stores/searchMenuControllerStore";
+import { BiArrowBack } from "react-icons/bi";
 
 export const RequestMenu: FC = () => {
   const [availableMeetingRooms, setAvailableMeetingRooms] = useState<
     MeetingRoom[]
   >([]);
   const openMenus = useMenuControllerStore((state) => state.openMenus);
-  const selectedHotel = useHotelSearchStore((state) => state.hotel);
 
   const selectedMeetingRoom = useSearchMenuControllerStore(
     (state) => state.searchMenuSelectedMeetingRoom
@@ -29,44 +28,16 @@ export const RequestMenu: FC = () => {
   );
   useEffect(() => {
     if (!openMenus.includes("request")) return setAvailableMeetingRooms([]);
-    const searchUrl = new URL(`http://localhost:3000/hotels/${selectedHotel}`);
-
+    const searchUrl = new URL(`http://localhost:3000/meeting-rooms`);
+    console.log(searchUrl);
     fetch(searchUrl.href)
       .then((res) => res.json())
       .then((data) => setAvailableMeetingRooms(data));
   }, [openMenus]);
 
   return (
-    // <Menu title={"Our recommendation"} name={"request"}>
-    //   <section className="flex flex-col gap-16">
-    //     {selectedSubMenu === "selection" && (
-    //       <>
-    //         {/* If room search has completed (has rooms) */}
-    //         {availableMeetingRooms.length ? (
-    //           availableMeetingRooms.map((meetingRoom) => (
-    //             <MeetingRoomsShowcaseCard
-    //               meetingRoom={meetingRoom}
-    //               key={meetingRoom.description}
-    //               onClick={() => {
-    //                 // setSelectedMeetingRoom(meetingRoom);
-    //                 setSelectedSubMenu("meetingRoomInfo");
-    //               }}
-    //             />
-    //           ))
-    //         ) : (
-    //           // If room search has not completed, show loading spinner
-    //           <div className="w-full h-full flex justify-center items-center">
-    //             <LoadingSpinner />
-    //           </div>
-    //         )}
-    //       </>
-    //     )}
-    //   </section>
-    // </Menu>
     <Menu title="Our recommendation" name="request" large>
       {selectedSubMenu === "selection" ? (
-        // selection: Room picker
-
         <>
           {/* If room search has completed (has rooms) */}
           {availableMeetingRooms.length ? (
@@ -87,7 +58,7 @@ export const RequestMenu: FC = () => {
             </div>
           )}
         </>
-      ) : selectedSubMenu === "roomInfo" ? (
+      ) : selectedSubMenu === "meetingRoomInfo" ? (
         // roomInfo: General information about the selected room
         <>
           <div
@@ -96,25 +67,36 @@ export const RequestMenu: FC = () => {
               setSelectedMeetingRoom(null);
               setSelectedSubMenu("selection");
             }}
-          ></div>
-          <div className="flex flex-col gap-12">
+          >
+            <BiArrowBack />
+          </div>
+          <div className="flex flex-col gap-2">
+            <img
+              src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/m%C3%B8der_konferencer/comwell_moede-og-konference41.jpg/636f34e0fcca9da5c8a8dde859a5bf2f.webp"
+              alt="meeting room"
+            />
             <div className="flex px-12 py-2">
               <section className="w-1/2">
-                <h2>{selectedMeetingRoom?.maxCapacity}</h2>
+                <p>
+                  Meeting room capacity:{" "}
+                  <i>{selectedMeetingRoom?.maxCapacity} people</i>
+                </p>
               </section>
               <section className="w-1/2 flex flex-col gap-8">
                 <p>{selectedMeetingRoom?.description}</p>
-                <p>{selectedMeetingRoom?.maxCapacity}</p>
+
+                <ul className="flex w-full h-64s items-center gap-4 overflow-auto">
+                  {selectedMeetingRoom?.bulletPoints.map((bulletPoint) => (
+                    <li>{bulletPoint}</li>
+                  ))}
+                </ul>
               </section>
-              <ul className="flex w-full h-64s items-center gap-4 overflow-auto">
-                {selectedMeetingRoom?.bulletPoints.map((bulletPoint) => (
-                  <li>{bulletPoint}</li>
-                ))}
-              </ul>
             </div>
           </div>
         </>
-      ) : null}
+      ) : (
+        <p>TEST</p>
+      )}
     </Menu>
   );
 };
